b6402cf3b0afccf4687141b793354fc02aa82e11
AtoosaHosseinikhah74 <36026281+AtoosaHosseinikhah74@users.noreply.github.com>
2018-07-23 01:07:02 +0430

    a
    aaa

---
+++ CrazyCube 2DTest V0.0/Assets/Scripts/Main/CheckPointController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour {
+
+	// public Sprite Flag;
+	// public Sprite GreenFlag;
+	// private SpriteRenderer checkpointSpriteRenderer;
+	// public bool checkpointReached;
+
+
+	// void Start () 
+	// {
+	//    checkpointSpriteRenderer = GetComponent<SpriteRenderer>();
+	// }
+
+	// void OnTriggerEnter2D(Collider2D Other)
+	// {
+	// 	if(Other.tag == "WhitePlayer" || Other.tag =="RedPlayer" || Other.tag =="BluePlayer")
+	// 	{
+	// 		checkpointSpriteRenderer.sprite = GreenFlag;
+	// 		checkpointReached = true;
+	// 	}
+
+    // }
+}
---
+++ CrazyCube 2DTest V0.0/Assets/Scripts/Main/Coin/CoinScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+	// void Start()
+	// {
+	// 	coinTotal = PlayerPrefs.GetInt ("Coins");
+	// }
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		int Coins = PlayerPrefs.GetInt("Coins");
+		ScoreTextScript.CoinAmount +=1;
+		Destroy(gameObject);
+	}
+}
